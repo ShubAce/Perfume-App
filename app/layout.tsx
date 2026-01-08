8b99040ce82143cce0bfbc61d0b3e51e247400cf
ShubAce <Shubhamsuman2005@gmail.com>
2026-01-09 01:13:03 +0530

    vercel little change

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,8 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<Analytics />
+			<SpeedInsights />
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 				<Providers>{children}</Providers>
 			</body>
